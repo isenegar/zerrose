f632f1130a46612e6812d619ec82ffcb91555543
isenegar <isenegar@gmail.com>
2014-10-15 23:17:36 +1000

    jfh

---
+++ zerrose/createworlddialog.cpp
@@ -0,0 +1,6 @@
+#include "createworlddialog.h"
+
+CreateWorldDialog::CreateWorldDialog(QWidget *parent) :
+    QDialog(parent)
+{
+}
---
+++ zerrose/createworlddialog.h
@@ -0,0 +1,18 @@
+#ifndef CREATEWORLDDIALOG_H
+#define CREATEWORLDDIALOG_H
+
+#include <QDialog>
+
+class CreateWorldDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit CreateWorldDialog(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // CREATEWORLDDIALOG_H
